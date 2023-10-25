f3574f92cd15e210d75f0a8383406cf33814cc53
jeongsaeyeong <140478779+jeongsaeyeong@users.noreply.github.com>
2023-10-25 14:50:59 +0900

    Add files via upload

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,32 +1,23 @@
 import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { fetchFromAPI } from '../utils/api';
-
-const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-}
+import VideoSearch from '../components/video/VideoSearch';
 
 const Channel = () => {
     const { channelId } = useParams();
-    const [channelDetail, setChannerDetail] = useState();
-    const [videos, setVideo] = useState([]);
+    const [ channelDetail, setChannerDetail] = useState();
+    const [ channelVideo, setchannelVideo ] = useState([]);
 
     useEffect(() => {
         const fetchResults = async () => {
             try {
-                // 채널 데이터 
                 const channeldata = await fetchFromAPI(`channels?part=snippet&id=${channelId}`);
-                console.log(channeldata.items)
                 setChannerDetail(channeldata.items[0])
 
-                // 비디오 데이터 
-                const videodata = await fetchFromAPI(`search?type=video&part=snippet&q=${channelId}`);
-                console.log(videodata)
-                setVideo(videodata.items)
+                const videosData = await fetchFromAPI(`search?channelId=${channelId}&part=snippet&order=date`);
+                console.log(videosData);
+                setchannelVideo(videosData.items);
+
             } catch (error) {
                 console.log("error fetching data", error);
             }
@@ -52,35 +43,10 @@ const Channel = () => {
                             <span>조회수 {channelDetail.statistics.viewCount}</span>
                         </div>
                     </div>
-                    <div className="channel__video video__inner"></div>
-                    <div className="channel__more"></div>
-                    <div className="video__item">
-                        <div className='video__inner search'>
-                            {videos.map((video, key) => (
-                                <div className='video' key={key}>
-                                    <div className='video__thumb play__icon'>
-                                        <Link
-                                            to={`/video/${video.id.videoId}`}
-                                            style={{ backgroundImage: `url(${video.snippet.thumbnails.high.url})` }}
-                                        >
-                                        </Link>
-                                    </div>
-                                    <div className='video__info'>
-                                        <div className='title'>
-                                            <Link to={`/video/${video.id.videoId}`}>{video.snippet.title}</Link>
-                                        </div>
-                                        <div className="desc">
-                                            {video.snippet.description}
-                                        </div>
-                                        <div className='info'>
-                                            <Link to={`/channel/${video.snippet.channelId}`} className='author'>{video.snippet.channelTitle}</Link>
-                                            <span className='date'>{formatDate(video.snippet.publishedAt)}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
+                    <div className="channel__video video__inner">
+                        <VideoSearch videos={channelVideo}/>
                     </div>
+                    <div className="channel__more"></div>
                 </div>
             )}
         </section>
